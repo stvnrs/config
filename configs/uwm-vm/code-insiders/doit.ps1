2b2919a0e2d49f4b05bd05508f1a10c6a9a4642f
Steven ROSE <steven@eightsix.io>
2024-09-23 16:27:10 +0100

    Updated configs\uwm-vm\code-insiders\doit.ps1

--- configs/uwm-vm/code-insiders/doit.ps1
+++ configs/uwm-vm/code-insiders/doit.ps1
@@ -1,3 +1,12 @@
 $ErrorActionPreference = 'Stop'
 $InformationPreference = 'Continue'
 
+$Extensions = @(
+    'ms-toolsai.jupyter'
+    'ms-python.python' 
+    'mechatroner.rainbow-csv'
+)
+
+$Extensions | ForEach-Object {
+    code-insiders --install-extension $_
+}
