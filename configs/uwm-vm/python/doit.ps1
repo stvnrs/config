06c158c69936443aad6c7e75f68302ac098a61e7
Steven ROSE <steven@eightsix.io>
2024-09-23 16:07:28 +0100

    Updated configs/uwm-vm/python/doit.ps1

--- configs/uwm-vm/python/doit.ps1
+++ configs/uwm-vm/python/doit.ps1
@@ -1,7 +1,7 @@
 $ErrorActionPreference = 'Stop'
 $InformationPreference = 'Continue'
 
-$Proxy = ([System.Net.WebRequest]::GetSystemWebProxy('https://pypi.org')).OriginalString
+$Proxy = ([System.Net.WebRequest]::GetSystemWebProxy().GetProxy('https://pypi.org')).OriginalString
 
 pip install ipykernel --proxy $Proxy
 pip install pandas --proxy $Proxy
