4a1aac89f743b3c5392b093d5e53a4fc03dea036
Steven ROSE <steven@eightsix.io>
2024-09-23 16:36:38 +0100

    Updated configs\uwm-vm\code-insiders\doit.ps1

--- configs/uwm-vm/code-insiders/doit.ps1
+++ configs/uwm-vm/code-insiders/doit.ps1
@@ -1,6 +1,22 @@
 $ErrorActionPreference = 'Stop'
 $InformationPreference = 'Continue'
 
+
+$Proxy = ([System.Net.WebRequest]::GetSystemWebProxy().GetProxy('https://marketplace.visualstudio.com/')).OriginalString
+
+$Settings = @"
+{
+    "http.proxySupport": "on",
+    "http.proxy": "$Proxy",
+    "http.proxtStrictSSL": "false",
+    "http.proxyAuthorization": null
+}
+"@
+
+$SettingsFilePath = "$Env:LOCALAPPDATA/Code - Insiders/User/settingses.json"
+
+$Settings | Out-File $SettingsFilePath  
+
 $Extensions = @(
     'ms-toolsai.jupyter'
     'ms-python.python' 
