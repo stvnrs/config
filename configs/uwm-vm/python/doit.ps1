60fbc3262c4ce1a8699abd0436f3c766841a9e1d
Steven ROSE <steven@eightsix.io>
2024-09-25 15:46:12 +0100

    Updated configs\uwm-vm\python\doit.ps1
    Updated python modules

--- configs/uwm-vm/python/doit.ps1
+++ configs/uwm-vm/python/doit.ps1
@@ -1,7 +1,18 @@
 $ErrorActionPreference = 'Stop'
 $InformationPreference = 'Continue'
 
+$Modules = @(
+    'ipykernel'
+    'pandas'
+    'openpyxl'
+)
+    
 $Proxy = ([System.Net.WebRequest]::GetSystemWebProxy().GetProxy('https://pypi.org')).OriginalString
 
-pip install ipykernel --proxy $Proxy
-pip install pandas --proxy $Proxy
+$Modules | ForEach-Object {
+    if($null -eq $Proxy) {
+        pip install $_ 
+    } else {
+        pip install $_  --proxy $($Proxy.OriginalString)
+    }
+}
