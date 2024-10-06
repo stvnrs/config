6b407630edc4fae44e5921317a477da7dd18ce4f
Steven ROSE <steven@eightsix.io>
2024-10-06 13:35:45 +0100

    wip

--- configs/uwm-vm/git/doit.ps1
+++ configs/uwm-vm/git/doit.ps1
@@ -4,8 +4,6 @@ $PSNativeCommandUseErrorActionPreference = $true
 
 $Proxy = ([System.Net.WebRequest]::GetSystemWebProxy().GetProxy('https://git-scm.com')).OriginalString
 
-
-
 if($Proxy.Length -gt 0){
     git config --global http.proxy $Proxy
 # } else {
@@ -15,11 +13,11 @@ if($Proxy.Length -gt 0){
 
 $Env:BS_SECTION_CHAR * $Host.UI.RawUI.WindowSize.Width 
 "Config user name & email..."
-$Username = $Env:BS_USER_NAME ?? (Read-Host -Prompt "Enter your user name for git logs (set `$Env:BS_USER_NAME to avoid this prompt in future)")
+$Username = $Env:PAVE_USER_NAME ?? (Read-Host -Prompt "Enter your user name for git logs (set `$Env:BS_USER_NAME to avoid this prompt in future)")
 Write-Information "Setting git user.name to $Username"
 git config --global user.name $Username 
 
-$Email = $Env:BS_USER_EMAIL ?? (Read-Host -Prompt "Enter your email name for git logs (set `$Env:BS_USER_EMAIL to avoid this prompt in future)")
+$Email = $Env:PAVE_USER_EMAIL ?? (Read-Host -Prompt "Enter your email name for git logs (set `$Env:BS_USER_EMAIL to avoid this prompt in future)")
 Write-Information "Setting git user.email to $Email"
 git config --global user.email $Email
 "Config user name & email✔️"
