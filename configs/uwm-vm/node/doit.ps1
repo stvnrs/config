2283fd57b71e42c1d1b5a8f0b6f7df0953ed2d2c
Steven Rose <stvnrs@users.noreply.github.com>
2025-01-16 23:35:33 +0000

    Update doit.ps1

--- configs/uwm-vm/node/doit.ps1
+++ configs/uwm-vm/node/doit.ps1
@@ -1,1 +1,10 @@
-#
+$ErrorActionPreference = 'Stop'
+$InformationPreference = 'Continue'
+$PSNativeCommandUseErrorActionPreference = $true
+
+"Configuring node..."
+
+npm install azure-functions-core-tools@4 --global --unsafe=true
+"+ azure-functions-core-tools@4 added to global packages"
+
+"Configuring node ✔️"
