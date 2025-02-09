ba4bee6cabfdce43598f51c0f5c1e0037a2fe20b
Steven Rose <stvnrs@users.noreply.github.com>
2025-02-09 14:00:02 +0000

    Merging
    * Updated configs\uwm-vm\pwsh\doit.ps1
    
      - removed psduckdb from default modules
    
    * Updated configs\uwm-vm\terminal\settings.json
    
      - Changed to reply onn PWSH_DEFAULT_PATH for path to pwsh
    
    * Added config for vs code
    
    * Updated whitespace in configs\uwm-vm\code-insiders\doit.ps1

---
+++ configs/uwm-vm/code/doit.ps1
@@ -0,0 +1,53 @@
+$ErrorActionPreference = 'Stop'
+$InformationPreference = 'Continue'
+
+$Proxy = ([System.Net.WebRequest]::GetSystemWebProxy().GetProxy('https://dev.azure.com/')).OriginalString
+
+"Deploying settings..."
+
+$Settings = @"
+{
+    "http.proxySupport": "on",
+    "http.proxy": "$Proxy",
+    "http.proxtStrictSSL": "false",
+    "http.proxyAuthorization": null
+    "window.zoomLevel": 0.4,
+    "workbench.colorTheme": "Solarized Dark"
+}
+"@
+
+"Deploying settings✔️"
+
+$SettingsPath = "$Env:APPDATA/Code/User"
+$SettingsFilePath = "$SettingsPath/settings.json"
+
+if(!(Test-Path $SettingsPath)){
+    mkdir $SettingsPath
+}
+
+$Settings | Out-File $SettingsFilePath  
+
+$Env:BS_SECTION_CHAR * $Host.UI.RawUI.WindowSize.Width 
+"Installing extensions..."
+
+$Extensions = @(
+    'GitHub.remotehub'
+    'mechatroner.rainbow-csv'
+    'ms-azure-devops.azure-pipelines'
+    'ms-azuretools.vscode-azurefunctions'
+    'ms-azuretools.vscode-azureresourcegroups'
+    'ms-azuretools.vscode-bicep'
+    'ms-dotnettools.vscode-dotnet-runtime'
+    'ms-python.python' 
+    'ms-toolsai.datawrangler'
+    'ms-toolsai.jupyter'
+    'ms-vscode.azure-repos'
+    'ms-vscode.powershell'
+    'ms-vscode.remote-repositories'
+)
+
+$Extensions | ForEach-Object {
+    code-insiders --install-extension $_
+}
+
+"Installing extensions✔️"
